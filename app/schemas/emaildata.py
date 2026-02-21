d1ac093ed58cf6dd0466b652864dc06243e0f22c
revego <info@citylog.cloud>
2026-02-21 21:20:51 +0000

    update with new endpoint to extract massive mail for user

--- app/schemas/emaildata.py
+++ app/schemas/emaildata.py
@@ -59,6 +59,17 @@ class EmailDataPublic(BaseModel):
     class Config:
         from_attributes = True
 
+class SegnalazioneOut(BaseModel):
+    id: int
+    typo: Optional[str] = None
+    address: Optional[str] = None
+    immage_url: Optional[str] = None
+    image_time: datetime
+    user_id: int
+
+    class Config:
+        from_attributes = True
+
 class FacebookAuthRequest(BaseModel):
     access_token: str
 
